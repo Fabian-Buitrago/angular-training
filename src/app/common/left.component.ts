562e2c05beb3ce1841e2cec36358ae811cc94f17
Fabian Buitrago <fabianbuitragov@gmail.com>
2017-05-15 22:45:37 -0500

    Css styles

--- src/app/common/left.component.ts
+++ src/app/common/left.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-left',
-    templateUrl: './left.component.html'
+    templateUrl: './left.component.html',
+    styleUrls: ['./left.component.css']
 })
 export class LeftComponent {
 
--- src/app/common/right.component.ts
+++ src/app/common/right.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-right',
-    templateUrl: './right.component.html'
+    templateUrl: './right.component.html',
+    styleUrls: ['./right.component.css']
 })
 export class RightComponent {
 
